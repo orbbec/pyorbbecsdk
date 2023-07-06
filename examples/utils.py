9a382e2c3d23babb55bc650676b33d08a7ebf4c2
Joe Dong <jian.dong@pobox.com>
2023-07-06 11:31:32 +0800

    fix frame to bgr image

--- examples/utils.py
+++ examples/utils.py
@@ -44,22 +44,32 @@ def frame_to_bgr_image(frame: VideoFrame) -> Union[Optional[np.array], Any]:
     height = frame.get_height()
     color_format = frame.get_format()
     data = np.asanyarray(frame.get_data())
-    image = np.resize(data, (height, width, 3))
+    image = np.zeros((height, width, 3), dtype=np.uint8)
     if color_format == OBFormat.RGB:
+        image = np.resize(data, (height, width, 3))
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     elif color_format == OBFormat.BGR:
+        image = np.resize(data, (height, width, 3))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     elif color_format == OBFormat.YUYV:
+        image = np.resize(data, (height, width, 2))
         image = cv2.cvtColor(image, cv2.COLOR_YUV2BGR_YUYV)
     elif color_format == OBFormat.MJPG:
         image = cv2.imdecode(data, cv2.IMREAD_COLOR)
     elif color_format == OBFormat.I420:
+        image = np.resize(data, (height, width * 3 // 2))
         image = cv2.cvtColor(image, cv2.COLOR_YUV2BGR_I420)
+        image = image[:, :width, :]
     elif color_format == OBFormat.NV12:
+        image = np.resize(data, (height, width * 3 // 2))
         image = cv2.cvtColor(image, cv2.COLOR_YUV2BGR_NV12)
+        image = image[:, :width, :]
     elif color_format == OBFormat.NV21:
+        image = np.resize(data, (height, width * 3 // 2))
         image = cv2.cvtColor(image, cv2.COLOR_YUV2BGR_NV21)
+        image = image[:, :width, :]
     elif color_format == OBFormat.UYVY:
+        image = np.resize(data, (height, width * 2))
         image = cv2.cvtColor(image, cv2.COLOR_YUV2BGR_UYVY)
     else:
         print("Unsupported color format: {}".format(color_format))

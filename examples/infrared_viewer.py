aa68e8eac618e39ce73e245b5747c4cedd7806b7
Joe Dong <jian.dong@pobox.com>
2023-07-31 11:06:16 +0800

    fix ir render

--- examples/infrared_viewer.py
+++ examples/infrared_viewer.py
@@ -37,17 +37,26 @@ def main():
             ir_format = ir_frame.get_format()
             if ir_format == OBFormat.Y8:
                 ir_data = np.resize(ir_data, (height, width, 1))
+                data_type = np.uint8
+                image_dtype = cv2.CV_8UC1
+                max_data = 255
             elif ir_format == OBFormat.MJPG:
                 ir_data = cv2.imdecode(ir_data, cv2.IMREAD_UNCHANGED)
+                data_type = np.uint8
+                image_dtype = cv2.CV_8UC1
+                max_data = 255
                 if ir_data is None:
                     print("decode mjpeg failed")
                     continue
                 ir_data = np.resize(ir_data, (height, width, 1))
             else:
                 ir_data = np.frombuffer(ir_data, dtype=np.uint16)
+                data_type = np.uint16
+                image_dtype = cv2.CV_16UC1
+                max_data = 65535
                 ir_data = np.resize(ir_data, (height, width, 1))
-            cv2.normalize(ir_data, ir_data, 0, 255, cv2.NORM_MINMAX)
-            ir_data = ir_data.astype(np.uint8)
+            cv2.normalize(ir_data, ir_data, 0, max_data, cv2.NORM_MINMAX, dtype=image_dtype)
+            ir_data = ir_data.astype(data_type)
             ir_image = cv2.cvtColor(ir_data, cv2.COLOR_GRAY2RGB)
             cv2.imshow("Infrared Viewer", ir_image)
             key = cv2.waitKey(1)

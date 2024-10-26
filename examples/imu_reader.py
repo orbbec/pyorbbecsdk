d7521438dd63c447afcb113b6f5d1cfbcc326495
zhonghong <zhonghong@orbbec.com>
2024-10-26 16:20:32 +0800

    update imu sample，use Pipiline get imu data

--- examples/imu_reader.py
+++ examples/imu_reader.py
@@ -13,90 +13,48 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # ******************************************************************************
-from pyorbbecsdk import *
 import time
-from threading import Lock
+import cv2
+import numpy as np
+from pyorbbecsdk import *
 
-console_lock = Lock()
-stop_gyro = False
-stop_accel = False
-
-
-def on_gyro_frame_callback(frame: Frame):
-    if frame is None:
-        return
-    global stop_gyro
-    if stop_gyro:
-        return
-    with console_lock:
-        gyro_frame: GyroFrame = frame.as_gyro_frame()
-        if gyro_frame is not None:
-            print("GyroFrame: ts={}".format(gyro_frame.get_timestamp()))
-            print("GyroFrame: x={}, y={}, z={}".format(gyro_frame.get_x(), gyro_frame.get_y(),
-                                                       gyro_frame.get_z()))
-
-
-def on_accel_frame_callback(frame: Frame):
-    if frame is None:
-        return
-    global stop_accel
-    if stop_accel:
-        return
-    with console_lock:
-        accel_frame: AccelFrame = frame.as_accel_frame()
-        if accel_frame is not None:
-            print("AccelFrame: ts={}".format(accel_frame.get_timestamp()))
-            print("AccelFrame: x={}, y={}, z={}".format(accel_frame.get_x(), accel_frame.get_y(),
-                                                        accel_frame.get_z()))
-
+ESC_KEY = 27
 
 def main():
-    ctx: Context = Context()
-    device_list: DeviceList = ctx.query_devices()
-    if device_list.get_count() == 0:
-        print("No device connected")
-        return
-    device: Device = device_list.get_device_by_index(0)
-    sensor_list: SensorList = device.get_sensor_list()
-    try:
-        gyro_senor = sensor_list.get_sensor_by_type(OBSensorType.GYRO_SENSOR)
-        if gyro_senor is None:
-            print("No gyro sensor")
-            return
-        gyro_profile_list: StreamProfileList = gyro_senor.get_stream_profile_list()
-        gyro_profile: StreamProfile = gyro_profile_list.get_stream_profile_by_index(0)
-        assert gyro_profile is not None
-        gyro_senor.start(gyro_profile, on_gyro_frame_callback)
-    except OBError as e:
-        print(e)
-        return
-    try:
-        accel_sensor = sensor_list.get_sensor_by_type(OBSensorType.ACCEL_SENSOR)
-        if accel_sensor is None:
-            print("No accel sensor")
-            return
-        accel_profile_list: StreamProfileList = accel_sensor.get_stream_profile_list()
-        accel_profile: StreamProfile = accel_profile_list.get_stream_profile_by_index(0)
-        assert accel_profile is not None
-        accel_sensor.start(accel_profile, on_accel_frame_callback)
-    except OBError as e:
-        print(e)
-        return
+    config = Config()
+    pipeline = Pipeline()
+    config.enable_accel_stream()
+    config.enable_gyro_stream()
+    config.set_frame_aggregate_output_mode(OBFrameAggregateOutputMode.FULL_FRAME_REQUIRE)
+    pipeline.start(config)
     while True:
         try:
-            time.sleep(1)
-        except KeyboardInterrupt:
-            # Don't forget to stop the sensor
-            global stop_accel, stop_gyro
-            stop_accel = True
-            stop_gyro = True
-            time.sleep(0.01)
-            if gyro_senor is not None:
-                gyro_senor.stop()
-            if accel_sensor is not None:
-                accel_sensor.stop()
-            break
+            frames = pipeline.wait_for_frames(100)
+            if frames is None:
+                continue
+            
+            accel_frame = frames.get_frame(OBFrameType.ACCEL_FRAME)
+            accel_frame = accel_frame.as_accel_frame()
+            
+            if accel_frame is not None:
+                accel_index = accel_frame.get_index()
+                if accel_index % 50 == 0:
+                    print("AccelFrame: ts={}".format(accel_frame.get_timestamp()))
+                    print("AccelFrame: x={}, y={}, z={}".format(accel_frame.get_x(), accel_frame.get_y(),accel_frame.get_z()))    
 
+            gyro_frame = frames.get_frame(OBFrameType.GYRO_FRAME)
+            gyro_frame = gyro_frame.as_gyro_frame()
+            if gyro_frame is not None:
+                gyro_index = gyro_frame.get_index()
+                if gyro_index % 50 == 0:
+                    print("GyroFrame: ts={}".format(gyro_frame.get_timestamp()))
+                    print("GyroFrame: x={}, y={}, z={}".format(gyro_frame.get_x(), gyro_frame.get_y(),gyro_frame.get_z()))  
+
+            key = cv2.waitKey(1)
+            if key == ord('q') or key == ESC_KEY:
+                break            
+        except KeyboardInterrupt:
+            break           
 
 if __name__ == "__main__":
     main()

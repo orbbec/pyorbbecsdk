e2543b3935e772ebbb73cf9b5a6896d3ab1f61b0
Joe Dong <jian.dong@pobox.com>
2024-11-16 13:58:16 +0800

    Update copyright

--- src/pyorbbecsdk/frame.cpp
+++ src/pyorbbecsdk/frame.cpp
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2023 Orbbec 3D Technology, Inc
+ * Copyright (c) 2024 Orbbec 3D Technology, Inc
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.

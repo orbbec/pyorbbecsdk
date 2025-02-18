96d305402e40f1bfef66bec518fee27ec893811e
shanpang <shanpang@orbbec.com>
2025-02-18 03:14:58 +0000

    [Update] Stash working linux build sh;
    - Stash working linux build sh;

--- scripts/build_linux_whl.sh
+++ scripts/build_linux_whl.sh
@@ -41,8 +41,8 @@ rsync -av --exclude='*.cmake' "$SRC_DIR/" ./install/lib/
 # Copy examples to /install/lib
 cp -r ./examples ./install/lib/pyorbbecsdk
 cp ./requirements.txt ./install/lib/pyorbbecsdk/examples
-# cp ./build/*.so ./install/lib/pyorbbecsdk/examples
-# rsync -av --exclude='*.cmake' "$SRC_DIR/" ./install/lib/pyorbbecsdk/examples
+cp ./build/*.so ./install/lib/pyorbbecsdk/examples
+rsync -av --exclude='*.cmake' "$SRC_DIR/" ./install/lib/pyorbbecsdk/examples
 
 
 # Run Python setup.py to build a wheel package

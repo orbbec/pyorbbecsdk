1f09255f2ab18aa3662b154da31ef1582fbc86a7
zhougong <zhougong@orbbec.com>
2025-05-30 23:38:02 +0800

    Update build shell script

--- scripts/build_whl/build_linux_whl_local.sh
+++ scripts/build_whl/build_linux_whl_local.sh
@@ -13,6 +13,8 @@ which pybind11
 
 # Remove ./build directory
 rm -rf ./build
+# Remove old /install directory
+rm -rf ./install
 
 # Create build directory and navigate into it
 mkdir -p build && cd build
@@ -23,36 +25,18 @@ cmake -Dpybind11_DIR=$(pybind11-config --cmakedir) ..
 # Build with make using 4 threads
 make -j$(nproc)
 
+make install
+
 # Move back to the parent directory
 cd ..
 
-# Remove old /install directory and create new /install/lib directory
-rm -rf ./install
+# Create new /install/lib directory
 mkdir -p ./install/lib/pyorbbecsdk
-
-# Copy shared objects (*.so) from /build to /install/lib
-cp ./build/*.so ./install/lib/
-
-# Copy all files from /sdk/lib/arm64/ except *.cmake to /install/lib
-ARCH=$(uname -m)
-# Define source directory based on architecture
-if [ "$ARCH" == "aarch64" ]; then
-    SRC_DIR="./sdk/lib/arm64"
-elif [ "$ARCH" == "x86_64" ]; then
-    SRC_DIR="./sdk/lib/linux_x64"
-else
-    echo "Unsupported architecture: $ARCH"
-    exit 1
-fi
-rsync -av --exclude='*.cmake' "$SRC_DIR/" ./install/lib/
 
 # Copy examples to /install/lib
 cp -r ./examples ./install/lib/pyorbbecsdk
 cp -r ./config ./install/lib/pyorbbecsdk
 cp ./requirements.txt ./install/lib/pyorbbecsdk/examples
-cp ./build/*.so ./install/lib/pyorbbecsdk/examples
-rsync -av --exclude='*.cmake' "$SRC_DIR/" ./install/lib/pyorbbecsdk/examples
-
 
 # Run Python setup.py to build a wheel package
 python3 setup.py sdist bdist_wheel

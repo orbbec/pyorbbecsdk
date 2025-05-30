ace9b5ca67537a61c0027f830a9c9ddfb86dc46e
zhonghong <zhonghong@orbbec.com>
2025-05-30 21:38:54 +0800

    Update env.sh

--- env.sh
+++ env.sh
@@ -4,3 +4,4 @@ CURR_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
 
 # shellcheck disable=SC2155
 export PYTHONPATH=${CURR_DIR}/install/lib:$PYTHONPATH
+export LD_LIBRARY_PATH=${CURR_DIR}/install/lib:$LD_LIBRARY_PATH
